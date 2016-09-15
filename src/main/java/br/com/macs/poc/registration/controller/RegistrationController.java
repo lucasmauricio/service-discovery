80db9bf8f75cef12ef057c4a7bb4e2f8b434a8fd
lucasmauricio <lucasmauricio@yahoo.com.br>
2016-09-15 15:34:37 -0300

    refactoring: putting data only at the repository

--- src/main/java/br/com/macs/poc/registration/controller/RegistrationController.java
+++ src/main/java/br/com/macs/poc/registration/controller/RegistrationController.java
@@ -1,13 +1,7 @@
 package br.com.macs.poc.registration.controller;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -22,12 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import br.com.macs.poc.registration.controller.dto.AssetDto;
 import br.com.macs.poc.registration.repository.RegistrationRepository;
 
@@ -36,77 +24,23 @@ public class RegistrationController {
 
 	private static final Logger logger = LoggerFactory.getLogger(RegistrationController.class);
 
-	private static Map<String, AssetDto> registeredAssets;
-
 	@Autowired
 	private RegistrationRepository registrationRepository;
 
-	@PostConstruct
-	public void init() {
-		registeredAssets = new HashMap<String, AssetDto>();
-
-		logger.debug("Trying to read persisted data");
-
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			String retrievedData = registrationRepository.readData();
-
-			if (!retrievedData.isEmpty()) {
-				registeredAssets = mapper.readValue(retrievedData, new TypeReference<HashMap<String, AssetDto>>() {
-				});
-				logger.info(String.format("%d asset(s) retrieved from repository", registeredAssets.size()));
-			} else {
-				logger.info("No data found in the repository");
-			}
-
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		logger.debug("Registration service initialized");
-	}
-
-	private void persistData() {
-		// TODO do some concurrency control
-		// to be simple, I think of using queues to read and write
-
-		// TODO do some hash check to avoid unnecessary write
-
-		ObjectMapper mapper = new ObjectMapper();
-		final StringWriter sw = new StringWriter();
-		try {
-			mapper.writeValue(sw, registeredAssets);
-
-			registrationRepository.writeData(sw.toString());
-		} catch (JsonGenerationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 	@RequestMapping(value = "/assets", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<AssetDto>> getRegisteredItens() {
-		logger.info("GET	/assets -> " + registeredAssets);
-		return new ResponseEntity<List<AssetDto>>(new ArrayList<AssetDto>(registeredAssets.values()), HttpStatus.OK);
+		logger.info("GET	/assets -> " + registrationRepository.getAssets());
+		
+//		return new ResponseEntity<List<AssetDto>>(new ArrayList<AssetDto>(registrationRepository.getRegisteredAssets().values()), HttpStatus.OK);
+		return new ResponseEntity<List<AssetDto>>(registrationRepository.getAssets(), HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/asset/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AssetDto> getRegisteredItem(@PathVariable String id) {
 		logger.info("GET	/asset/" + id);
-		AssetDto assetFound = registeredAssets.get(id);
+		
+		AssetDto assetFound = registrationRepository.getAssetById(id);
 		if (assetFound == null) {
 			logger.info("   -> 404 (asset not found)");
 			return new ResponseEntity<AssetDto>(HttpStatus.NOT_FOUND);
@@ -119,23 +53,24 @@ public class RegistrationController {
 	@RequestMapping(value = "/asset/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> registerItem(@RequestBody @Valid AssetDto putAsset, @PathVariable String id) {
 		logger.info("PUT	/asset/" + id + putAsset);
+		
 		putAsset.setId(id);
-		registeredAssets.put(id, putAsset);
-		persistData();
+		registrationRepository.putAsset(id, putAsset);
 		// TODO put some header data (i.e. the new item address)
+		
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "/asset/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AssetDto> deleteRegisteredItem(@PathVariable String id) {
 		logger.info("DELETE	/asset/" + id);
-		AssetDto assetToDel = registeredAssets.remove(id);
+		
+		AssetDto assetToDel = registrationRepository.removeAssetById(id);
 		if (assetToDel == null) {
 			logger.info("   -> 404 (asset not found)");
 			return new ResponseEntity<AssetDto>(HttpStatus.NOT_FOUND);
 		} else {
 			logger.info("   -> asset deleted: " + assetToDel);
-			persistData();
 			return new ResponseEntity<AssetDto>(HttpStatus.OK);
 		}
 	}
--- src/main/java/br/com/macs/poc/registration/repository/RegistrationRepository.java
+++ src/main/java/br/com/macs/poc/registration/repository/RegistrationRepository.java
@@ -1,21 +1,41 @@
 package br.com.macs.poc.registration.repository;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.macs.poc.registration.controller.dto.AssetDto;
+
 
 @Repository
 public class RegistrationRepository {
 
 	private static final String DATA_FILE_NAME = "assets-repository.json";
 	private static final Logger logger = LoggerFactory.getLogger(RegistrationRepository.class);
+
+	//TODO do not access the collection directly to avoid null pointers problems
+	private static Map<String, AssetDto> registeredAssets;
 
 	private File dataFile;
 
@@ -36,8 +56,40 @@ public class RegistrationRepository {
 	}
 	
 	
+	@PostConstruct
+	public void init() {
+		registeredAssets = new HashMap<String, AssetDto>();
+
+		logger.debug("Trying to read persisted data");
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String retrievedData = readDataFile();
+
+			if (!retrievedData.isEmpty()) {
+				registeredAssets = mapper.readValue(retrievedData, new TypeReference<HashMap<String, AssetDto>>() {
+				});
+				logger.info(String.format("%d asset(s) retrieved from repository", registeredAssets.size()));
+			} else {
+				logger.info("No data found in the repository");
+			}
+
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		logger.debug("Registration service initialized");
+	}
 	
-	public String readData() {
+	
+	private String readDataFile() {
 		logger.debug("Trying to read data from the file " + dataFile.getAbsolutePath());
 		
 	    StringBuilder contents = new StringBuilder();
@@ -73,7 +125,7 @@ public class RegistrationRepository {
 	
 	
 	
-	public void writeData(String content) {
+	private void writeData(String content) {
 		logger.debug("Trying to write data into the file: " + dataFile.getAbsolutePath());
 		FileWriter fw;
 		try {
@@ -89,4 +141,62 @@ public class RegistrationRepository {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+
+	private void doPersistData() {
+		// TODO do some concurrency control
+		// to be simple, I think of using queues to read and write
+
+		// TODO do some hash check to avoid unnecessary write
+
+		ObjectMapper mapper = new ObjectMapper();
+		final StringWriter sw = new StringWriter();
+		try {
+			mapper.writeValue(sw, registeredAssets);
+
+			writeData(sw.toString());
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+
+	public List<AssetDto> getAssets() {
+		return new ArrayList<AssetDto>(registeredAssets.values());
+	}
+
+
+
+	public AssetDto getAssetById(String id) {
+		return registeredAssets.get(id);
+	}
+
+
+
+	//TODO return true if it was successfully
+	public void putAsset(String id, AssetDto putAsset) {
+		registeredAssets.put(id, putAsset);
+		doPersistData();
+	}
+
+
+
+	public AssetDto removeAssetById(String id) {
+		AssetDto assetRemoved = registeredAssets.remove(id);
+		if (assetRemoved != null) {
+			doPersistData();
+		}
+		return assetRemoved;
+	}
+	
 }
